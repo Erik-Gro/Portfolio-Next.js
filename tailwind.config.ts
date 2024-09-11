d11f0e7d8f7894530bf39721587f92cdf9a17b4d
ericgro2000 <eric.gro.2000@gmail.com>
2024-09-11 18:56:23 +0200

    tailwind config, globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        generalsans: ['General Sans', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: {
+          DEFAULT: '#000',
+          100: '#010103',
+          200: '#0E0E10',
+          300: '#1C1C21',
+          500: '#3A3A49',
+          600: '#1A1A1A',
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          800: '#E4E4E6',
+          700: '#D6D9E9',
+          600: '#AFB0B6',
+          500: '#62646C',
+        },
+      },
+      backgroundImage: {
+        terminal: "url('/assets/terminal.png')",
       },
     },
   },
