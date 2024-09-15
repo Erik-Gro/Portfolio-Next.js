46f383c847f37358f7341484e0a99d245a3023ba
ericgro2000 <eric.gro.2000@gmail.com>
2024-09-15 16:29:06 +0200

    add dynamic import for globe

--- src/sections/about/About.tsx
+++ src/sections/about/About.tsx
@@ -1,18 +1,23 @@
-"use client"
+"use client";
+
+import { useEffect, useState } from 'react';
 import Button from '@/shared/components/button.tsx/Button';
-import { useState } from 'react';
-import Globe from 'react-globe.gl';
+import dynamic from 'next/dynamic';
+
+const Globe = dynamic(() => import('react-globe.gl'), { ssr: false });
 
 const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText('2000.eric.gro@gmail.com');
-    setHasCopied(true);
+    if (navigator?.clipboard) {
+      navigator.clipboard.writeText('2000.eric.gro@gmail.com');
+      setHasCopied(true);
 
-    setTimeout(() => {
-      setHasCopied(false);
-    }, 2000);
+      setTimeout(() => {
+        setHasCopied(false);
+      }, 2000);
+    }
   };
 
   return (
@@ -21,11 +26,10 @@ const About = () => {
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
             <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
-
             <div>
               <p className="grid-headtext">Hi, I’m Eric Gro</p>
               <p className="grid-subtext">
-                I love to build resposive and great looking UI's as well as building scallable and secure backends
+                I love to build responsive and great-looking UIs as well as build scalable and secure backends.
               </p>
             </div>
           </div>
@@ -34,12 +38,10 @@ const About = () => {
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
             <img src="assets/mohammad-rahmani-unsplash.jpg" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
-
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                applications
+                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable applications.
               </p>
             </div>
           </div>
@@ -48,16 +50,16 @@ const About = () => {
         <div className="col-span-1 xl:row-span-4">
           <div className="grid-container">
             <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-              <Globe
-                height={326}
-                width={326}
-                backgroundColor="rgba(0, 0, 0, 0)"
-                showAtmosphere
-                showGraticules
-                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                labelsData={[{ lat: 40, lng: -100, text: 'Kassel, Germany', color: 'white', size: 15 }]}
-              />
+                <Globe
+                  height={326}
+                  width={326}
+                  backgroundColor="rgba(0, 0, 0, 0)"
+                  showAtmosphere
+                  showGraticules
+                  globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                  bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                  labelsData={[{ lat: 40, lng: -100, text: 'Kassel, Germany', color: 'white', size: 15 }]}
+                />
             </div>
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
@@ -70,12 +72,10 @@ const About = () => {
         <div className="xl:col-span-2 xl:row-span-3">
           <div className="grid-container">
             <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
-
             <div>
               <p className="grid-headtext">My Passion for Coding</p>
               <p className="grid-subtext">
-                I love solving problems and building things through code. Programming isn&apos;t just my
-                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+                I love solving problems and building things through code. Programming isn&apos;t just my profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
               </p>
             </div>
           </div>
@@ -83,12 +83,7 @@ const About = () => {
 
         <div className="xl:col-span-1 xl:row-span-2">
           <div className="grid-container">
-            <img
-              src="assets/grid4.png"
-              alt="grid-4"
-              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
-            />
-
+            <img src="assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top" />
             <div className="space-y-2">
               <p className="grid-subtext text-center">Contact me</p>
               <div className="copy-container" onClick={handleCopy}>
