87025046fcaded8173cc31c6dbe3a98b75c8a3a4
ericgro2000 <eric.gro.2000@gmail.com>
2024-09-16 09:23:02 +0200

    ThreeObjectProps type change

--- src/sections/main/types/shared.ts
+++ src/sections/main/types/shared.ts
@@ -1,5 +1,6 @@
 export interface ThreeObjectProps {
-    position?: [number, number, number];
-    scale?:[number, number, number]
+    position?: [number, number, number]|number
+    rotation:[number, number, number]|number
+    scale?:[number, number, number]|number
   }
   
