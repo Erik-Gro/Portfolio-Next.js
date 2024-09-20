62f25bd0c870d745b1a09b3dbe5e20ab4ec936de
ericgro2000 <eric.gro.2000@gmail.com>
2024-09-20 12:37:18 +0200

    add snake game movile inputs

--- public/games/snake/snake.js
+++ public/games/snake/snake.js
@@ -33,6 +33,43 @@ f = 1
       break
     }
 });
+
+window.addEventListener("touchstart", function(e) {
+  touchStartX = e.changedTouches[0].screenX;
+  touchStartY = e.changedTouches[0].screenY;
+});
+
+window.addEventListener("touchend", function(e) {
+  touchEndX = e.changedTouches[0].screenX;
+  touchEndY = e.changedTouches[0].screenY;
+  handleSwipe();
+});
+
+function handleSwipe() {
+  let deltaX = touchEndX - touchStartX;
+  let deltaY = touchEndY - touchStartY;
+
+  if (f == 1) return;
+
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    if (deltaX > 0 && snakeDirection.y != -1) {
+      f = 1;
+      snakeDirection = { x: 0, y: 1 };
+    } else if (deltaX < 0 && snakeDirection.y != 1) {
+      f = 1;
+      snakeDirection = { x: 0, y: -1 };
+    }
+  } else {
+    if (deltaY > 0 && snakeDirection.x != -1) {
+      f = 1;
+      snakeDirection = { x: 1, y: 0 };
+    } else if (deltaY < 0 && snakeDirection.x != 1) {
+      f = 1;
+      snakeDirection = { x: -1, y: 0 };
+    }
+  }
+}
+
 function newapple() {
   let mbapple = {x:Math.floor(Math.random()*21)+1, y:Math.floor(Math.random()*21)+1}
   if (snakeBody.some(el => el.x == mbapple.x && el.y == mbapple.y )) {
