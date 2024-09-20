64ad85836927ce4f8104cbbed04e722fac5750a4
ericgro2000 <eric.gro.2000@gmail.com>
2024-09-20 13:48:04 +0200

    remove comment

--- src/sections/projects/components/DemoComputer.tsx
+++ src/sections/projects/components/DemoComputer.tsx
@@ -36,7 +36,7 @@ type GLTFResult = GLTF & {
 
 const DemoComputer = (props: DemoComputerProps) => {
   const group = useRef<Group>(null);  
-  const { nodes, materials } = useGLTF('/models/computer.glb') as GLTFResult;  //remove any
+  const { nodes, materials } = useGLTF('/models/computer.glb') as GLTFResult;  
 
   const txt = useVideoTexture(props.texture ? props.texture : '/textures/project/project1.mp4');
 
