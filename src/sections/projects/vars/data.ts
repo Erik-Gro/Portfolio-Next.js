016c7c131b410b9727912301f8a963a2814b8ccf
Erik-Gro <eric.gro.2000@gmail.com>
2024-11-01 20:05:47 +0100

    added Commingle project

--- src/sections/projects/vars/data.ts
+++ src/sections/projects/vars/data.ts
@@ -36,19 +36,18 @@ export const myProjects = [
       ],
     },
     {
-      title: 'Second project and so on...',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget diam et libero facilisis viverra. Fusce vel varius libero, sit amet cursus risus.',
-      subdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
-      href: 'https://github.com/ericgro2000',
-      texture: '/textures/project/Wiki.mov',
-      logo: '/assets/project-logo2.png',
+      title: 'Commingle',
+      desc: 'Experience seamless communication with real-time messaging, image sharing, and reactions. Engage in threads, collaborate, and stay connected in a user-friendly platform built for teams.',
+      subdesc: 'Invite team members effortlessly with a unique code-based system, reply to specific messages, and keep conversations organized with threaded discussions and reactions. Perfect for collaborative, real-time engagement across teams and projects.',
+      href: 'https://commingle-by-eric-gro.vercel.app/',
+      texture: '/textures/project/CommingleDemo.mp4',
+      logo: '/assets/project-logo4.png',
       logoStyle: {
-        backgroundColor: '#13202F',
-        border: '0.2px solid #17293E',
-        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
       },
-      spotlight: '/assets/spotlight2.png',
+      spotlight: '/assets/spotlight1.png',
       tags: [
         {
           id: 1,
@@ -72,4 +71,41 @@ export const myProjects = [
         },
       ],
     },
+    // {
+    //   title: 'Second project and so on...',
+    //   desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget diam et libero facilisis viverra. Fusce vel varius libero, sit amet cursus risus.',
+    //   subdesc:
+    //     'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+    //   href: 'https://github.com/ericgro2000',
+    //   texture: '/textures/project/Wiki.mov',
+    //   logo: '/assets/project-logo2.png',
+    //   logoStyle: {
+    //     backgroundColor: '#13202F',
+    //     border: '0.2px solid #17293E',
+    //     boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    //   },
+    //   spotlight: '/assets/spotlight2.png',
+    //   tags: [
+    //     {
+    //       id: 1,
+    //       name: 'React.js',
+    //       path: '/assets/react.svg',
+    //     },
+    //     {
+    //       id: 2,
+    //       name: 'TailwindCSS',
+    //       path: 'assets/tailwindcss.png',
+    //     },
+    //     {
+    //       id: 3,
+    //       name: 'TypeScript',
+    //       path: '/assets/typescript.png',
+    //     },
+    //     {
+    //       id: 4,
+    //       name: 'Framer Motion',
+    //       path: '/assets/framer.png',
+    //     },
+    //   ],
+    // },
   ];
