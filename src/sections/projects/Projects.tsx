69022a3c1288653eac9f8fb436d0472dfe24490b
Erik-Gro <eric.gro.2000@gmail.com>
2024-11-02 20:14:07 +0100

    2 projects are sufficient to remove this

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -49,11 +49,11 @@ const Projects = () => {
 
   return (
     <section className="c-space my-20" id="work">
-      <p className="head-text">My Selected Work
+      {/* <p className="head-text">My Selected Work
       <span style={{ display: 'inline-block', transform: 'rotate(-5deg)', color: 'white' }}>
         (To be expanded)
       </span>
-      </p>
+      </p> */}
 
       <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
         <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
